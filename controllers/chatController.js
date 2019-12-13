fdff29066edf403714ca065e13fc6f91b40ca12f
iMammal <morrisc@gmail.com>
2019-12-12 20:10:14 -0500

    doc gen setup

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -106,6 +106,9 @@ exports.addChat = async function (req, res) {
 
     console.log("typeof info:");
     console.log(typeof info);
+    console.log("info.length:");
+    console.log(info.length);
+
     //await emailSender.sendEmail('spoon.jeremy@gmail.com', approver['email'], 'Please approve the documents from Deeplaw', 'test', '<strong>There are documents from ' + req.user.username + '</strong>');
     console.log('<strong>There are documents from ' + req + '</strong>');
     var emailResult = await emailSender.sendEmail('chukhman@uic.edu', 'morrisc@gmail.com', 'Please approve the documents from Deeplaw', 'test', '<strong>There are documents from : + req.user.username + </strong>');
