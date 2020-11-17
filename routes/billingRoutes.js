f3bc98d3210cb85b1eadfe228e694c4d2db511c1
Tyler Pritchard <pritchard.tyler@gmail.com>
2020-11-16 22:52:59 -0800

    comment out billing routes keys

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,22 +1,22 @@
-const keys = require('../config/keys');
-const stripe = require('stripe')(keys.stripeSecretKey);
-const requireLogin = require('../middleware/requireLogin');
+// const keys = require('../config/keys');
+// const stripe = require('stripe')(keys.stripeSecretKey);
+// const requireLogin = require('../middleware/requireLogin');
 
-module.exports = app => {
-    app.post('/api/stripe', requireLogin, async (req, res) => {
-        if (!req.user) {
-            return res.status(401).send({error: 'You must log in'});
-        }
+// module.exports = app => {
+//     app.post('/api/stripe', requireLogin, async (req, res) => {
+//         if (!req.user) {
+//             return res.status(401).send({error: 'You must log in'});
+//         }
 
-        const charge = await stripe.charges.create({
-            amount: 5000,
-            currency: 'usd',
-            description: 'Legal Services',
-            source: req.body.id
-        });
-        req.user.hours += 1;
-        const user = await req.user.save();
+//         const charge = await stripe.charges.create({
+//             amount: 5000,
+//             currency: 'usd',
+//             description: 'Legal Services',
+//             source: req.body.id
+//         });
+//         req.user.hours += 1;
+//         const user = await req.user.save();
 
-        res.send(user);
-    });
-};
+//         res.send(user);
+//     });
+// };
